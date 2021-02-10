7ed9e2abb5caa3a8418460a1c701cf462fc115a1
Jua Jang <jua.jang@kakao.com>
2021-02-10 13:38:39 +0900

    Update Query

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ client.connect(function(err) {
   const db = client.db(dbName);
 
   insertDocuments(db, function() {
-    client.close();
+    indexCollection(db, function() {
+      client.close();
+    });
   });
 });
 
@@ -36,3 +38,51 @@ const insertDocuments = function(db, callback) {
     callback(result);
   });
 }
+
+const findDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs)
+    callback(docs);
+  });
+}
+
+const updateDocument = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Update document where a is 2, set b equal to 1
+  collection.updateOne({ a : 2 }
+    , { $set: { b : 1 } }, function(err, result) {
+      assert.equal(err, null);
+      assert.equal(1, result.result.n);
+      console.log("Updated the document with the field a equal to 2");
+      callback(result);
+    });
+}
+
+const removeDocument = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Delete document where a is 3
+  collection.deleteOne({ a : 3 }, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Removed the document with the field a equal to 3");
+    callback(result);
+  });
+}
+
+const indexCollection = function(db, callback) {
+  db.collection('documents').createIndex(
+    { "a": 1 },
+    null,
+    function(err, results) {
+      console.log(results);
+      callback();
+    }
+  );
+};
